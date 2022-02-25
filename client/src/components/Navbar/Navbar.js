c0fcc06597fea31f8ad3458af478e02a4750c575
gadaharsh <gada.hp@somaiya.edu>
2022-02-25 20:37:42 +0530

    removed warnings and used Link

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,58 +1,74 @@
-import './Navbar.scss';
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return (
-        <>
-            <header>
-                <div className="header">
-                    <nav className="navigation">
-                        <a href="#" className="navbar-logo">Logo</a>
-                        <div className="navbar-right">
-                            <a href="#">About</a>
-                            <a href="#">Blog</a>
-                            <a href="#">Contact</a>
-                        </div>
-                    </nav>
-                    <div className=" p-0 container">
-                        <div className="row">
-                            <div className="col-4">
-                                <div className="box">
-                                    <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="Cloudy Sky" />
-                                </div>
-                                {/* <div className="tagline-right"> */}
-                                {/* <div className="tagline-right-text">The future is for workers</div> */}
-                                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
-                                {/* </div> */}
-                            </div>
-                            <div className="col-4">
-                                <div className="box">
-                                    <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="Cloudy Sky" />
-                                </div>
-                                {/* <div className="tagline-right"> */}
-                                {/* <div className="tagline-right-text">The future is for workers</div> */}
-                                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
-                                {/* </div> */}
-                            </div>
-                            <div className="col-4">
-                                <div className="box">
-                                    <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="Cloudy Sky" />
-                                </div>
-                                {/* <div className="tagline-right"> */}
-                                {/* <div className="tagline-right-text">The future is for workers</div> */}
-                                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
-                                {/* </div> */}
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="video-container">
+  return (
+    <>
+      <header>
+        <div className="header">
+          <nav className="navigation">
+            <Link to="#" className="navbar-logo">
+              Logo
+            </Link>
+            <div className="navbar-right">
+              <Link to="#">About</Link>
+              <Link to="#">Blog</Link>
+              <Link to="#">Contact</Link>
+            </div>
+          </nav>
+          <div className=" p-0 container">
+            <div className="row">
+              <div className="col-4">
+                <div className="box">
+                  <img
+                    src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+                    className="img-responsive bg-img"
+                    alt="Cloudy Sky"
+                  />
+                </div>
+                {/* <div className="tagline-right"> */}
+                {/* <div className="tagline-right-text">The future is for workers</div> */}
+                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
+                {/* </div> */}
+              </div>
+              <div className="col-4">
+                <div className="box">
+                  <img
+                    src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+                    className="img-responsive bg-img"
+                    alt="Cloudy Sky"
+                  />
+                </div>
+                {/* <div className="tagline-right"> */}
+                {/* <div className="tagline-right-text">The future is for workers</div> */}
+                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
+                {/* </div> */}
+              </div>
+              <div className="col-4">
+                <div className="box">
+                  <img
+                    src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+                    className="img-responsive bg-img"
+                    alt="Cloudy Sky"
+                  />
+                </div>
+                {/* <div className="tagline-right"> */}
+                {/* <div className="tagline-right-text">The future is for workers</div> */}
+                {/* <img src="https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" className="img-responsive bg-img" alt="" /> */}
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+          {/* <div className="video-container">
                         <video autoplay loop muted id="video-bg">
 
                             <source src="http://bigcom.com/assets/2014/08/iChooseB.mp4" type="video/mp4" />
 
                         </video>
                     </div> */}
-                </div>
-            </header>
-            {/* <div className="portfolio-section">
+        </div>
+      </header>
+      {/* <div className="portfolio-section">
                 <div className="tagline-left">
                     <div className="tagline-left-text">Serving the waters that serve our state.</div>
                     <video autoplay muted loop className="tagline-video">
@@ -66,7 +82,7 @@ function Navbar() {
                     </video>
                 </div>
             </div> */}
-        </>
-    )
+    </>
+  );
 }
 export default Navbar;
