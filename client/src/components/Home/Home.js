cee1c12dcb10d9e7d9984c47b6cc0c448f7a9a81
gadaharsh <gada.hp@somaiya.edu>
2022-04-21 17:28:23 +0530

    footer

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -13,7 +13,11 @@ import {
   useNavigate,
 } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import LocationCard from "../LocationCard/LocationCard";
+import Footer from "../Footer/Footer";
 // import * as mdb from 'mdb-ui-kit';
+
+const imageLocation="https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
 
 function Home() {
   return (
@@ -54,36 +58,13 @@ function Home() {
               </div>
             </div>
           </div>
-          <div class="row">
-            <div className="col-4">
-              <div class="card wheretocard" style={{width: "25rem"}}>
-                <img class="card-img-top wheretocardimg" src="https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="Card image cap"/>
-                <div class="card-body">
-                  <h5 class="card-title">Mumbai</h5>
-                </div>
-              </div>
-            </div>
-            <div className="col-4">
-              <div class="card wheretocard" style={{width: "25rem"}}>
-                <img class="card-img-top wheretocardimg" src="https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="Card image cap"/>
-                <div class="card-body">
-                  <h5 class="card-title">Mumbai</h5>
-                </div>
-              </div>
-            </div>
-            <div className="col-4">
-              <div class="card wheretocard" style={{width: "25rem"}}>
-                <img class="card-img-top wheretocardimg" src="https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="Card image cap"/>
-                <div class="card-body">
-                  <h5 class="card-title">Mumbai</h5>
-                </div>
-              </div>
-            </div>
+          <div class="row container">
+            <LocationCard image={imageLocation} location="Mumbai"/>
+            <LocationCard image={imageLocation} location="Kashmir"/>
+            <LocationCard image={imageLocation} location="Chennai"/>
           </div>
         </div>
-        <div className="websitebanner d-flex justify-content-center align-items-end">
-          <div class="relative lg:absolute lg:bottom-32 lg:left-0 lg:right-0 mx-auto z-10 md:max-w-2xl lg:max-w-3xl lg:flex lg:justify-between text-center lg:text-left"><p class="text-white text-center lg:text-left text-lg lg:w-4/6">The results are in! Explore this year’s expert-approved list of must-see destinations, places, and unforgettable experiences guaranteed to inspire.</p><div class="flex flex-col items-center pb-16 lg:pb-0"><a href="https://www.lonelyplanet.com/best-in-travel" class="btn inline-block mt-10 lg:mt-0 mb-6 bg-white text-black flex-shrink-0" aria-hidden="true">Discover the Winners</a><button class="text-white text-sm underline" type="button">How we choose</button></div></div>
-        </div>
+        <Footer/>
       </div>
     </>
   );
