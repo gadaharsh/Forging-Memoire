d01d59631d46132764c50f3ef95a834030962c8b
unknown <madhurdixit37@gmail.com>
2022-04-23 00:35:14 +0530

    single destionation page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Navbar from './components/Navbar/Navbar'
+import DestBlogPage from './components/DestBlogPage/DestBlogPage'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/destpage" element={<DestBlogPage/>} />
         </Routes>
       </BrowserRouter>
 
